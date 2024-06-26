d5fb694cba2fb8b22985bcf879608b507005e7eb
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-26 02:36:40 +0200

    4th task.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test_db'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test_db'@'localhost';
