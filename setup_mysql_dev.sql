9de5e1129ceff450957382f71fff43789d024ac1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-26 02:27:00 +0200

    2nd task.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,3 +6,9 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privilages of database hbnb_dev to user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush the privileges to ensure all changes take effect
+FLUSH PRIVILEGES;
