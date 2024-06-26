bdb716c350fab457ce5a1224562d14831227b57f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-26 02:47:11 +0200

    4th task.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls == None:
+        if cls is None:
             return
         for obj in FileStorage.__objects:
             if cls is None or cls == obj.__class__.__name__:
@@ -50,7 +50,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
