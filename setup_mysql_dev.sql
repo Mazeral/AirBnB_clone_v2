55aed57afb876a3b4ecbae42d46ff62407a0e1e1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-26 02:33:20 +0200

    2nd task.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
--- Setup SQL server
-CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create user
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privilages of database hbnb_dev to user hbnb_dev
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
