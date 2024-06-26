7fa947b83c724aaac35012b00eda70d5ee6ab4c4
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-27 01:53:10 +0200

    User

--- models/user.py
+++ models/user.py
@@ -2,11 +2,20 @@
 """
 This module defines a class User that inherits from BaseModel and Base.
 """
+
 from models.base_model import BaseModel
 from os import environ
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
 
+# Get the storage engine type from the environment variable
 storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+# If the storage engine is set to 'db', use SQLAlchemy's declarative base
+if storage_engine == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class User(BaseModel, Base):
@@ -20,15 +29,16 @@ class User(BaseModel, Base):
         last_name (str): The last name of the user.
     """
 
+    # Define the columns for the User table if the storage engine is 'db'
     if storage_engine == "db":
-        # Define the columns for the User table if the storage engine is 'db'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
     else:
-        # Define the attributes for
-        # the User class if the storage engine is not 'db'
+        # Define the attributes for the User class if
+        # the storage engine is not 'db'
+        # These attributes are used for in-memory storage
         email = ''
         password = ''
         first_name = ''
