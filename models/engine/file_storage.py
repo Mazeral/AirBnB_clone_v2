bf47a828bb618147c9c43865d3d2153edeb2ae2d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-27 02:51:16 +0200

    Fixing file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,91 +1,58 @@
 #!/usr/bin/python3
-"""
-Module containing FileStorage class used for file storage
-"""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
-import models
 
 
 class FileStorage:
-    """
-    FileStorage class is used to serialize and deserialize instances
-    to and from a JSON file.
-
-    Attributes:
-        __file_path (str): The path to the JSON file.
-        __objects (dict): A dictionary that stores the serialized objects.
-    """
-
-    # Initialize the path to the JSON file and the objects dictionary
-    __file_path = "file.json"
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """
-        Returns a dictionary containing every object in storage.
-
-        Args:
-            cls (class, optional): If specified, returns a dictionary
-            containing only objects of the given class.
-
-        Returns:
-            dict: A dictionary containing the objects.
-        """
-        if not cls:
+        """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        else:
             return self.__objects
 
-        result = {}
-        for key in self.__objects.keys():
-            if key.split(".")[0] == cls.__name__:
-                result.update({key: self.__objects[key]})
-        return result
-
     def new(self, obj):
-        """
-        Creates a new object and saves it to the storage.
-
-        Args:
-            obj (object): The object to be saved.
-        """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """
-        Updates the JSON file to reflect any changes in the objects.
-        """
-        temp = {}
-        for id, obj in self.__objects.items():
-            temp[id] = obj.to_dict()
-        with open(self.__file_path, "w") as json_file:
-            json.dump(temp, json_file)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
-        """
-        Updates the objects dictionary to restore previously created objects.
-        """
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
-            with open(self.__file_path, "r") as json_file:
-                temp = json.load(json_file)
-            for id, dict in temp.items():
-                temp_instance = models.dummy_classes[dict["__class__"]](**dict)
-                self.__objects[id] = temp_instance
-        except:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
             pass
-
-    def close(self):
-        """
-        Displays the HBNB data by reloading the objects from the storage.
-        """
-        self.reload()
-
     def delete(self, obj=None):
-        """
-        Deletes an object from the storage if it exists.
-
-        Args:
-            obj (object, optional): The object to be deleted. If not specified,
-            the method does nothing.
-        """
-        if obj:
-            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
+        if obj is None:
+            return
+        if obj in self.__objects:
+            del (self.__objects[obj])
